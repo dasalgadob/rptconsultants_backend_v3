9cabdc5f3644e8ab9bccdad152b01dfee7988bd1
dsalgadob <dsalgado@zircon.tech>
2023-02-15 21:06:21 -0500

    Add validations and related tests for degree

--- app/models/reference_tables/degree.rb
+++ app/models/reference_tables/degree.rb
@@ -1,6 +1,7 @@
 class ReferenceTables::Degree < ApplicationRecord
-  validates :number, presence: true
-  validates :minimum, presence: true
-  validates :medium, presence: true
-  validates :maximum, presence: true
+  validates :number, uniqueness: true
+  validates :number, :minimum, :medium, :maximum, :hay_degrees, :ggs_degrees, presence: true
+  validates :number, comparison: { greater_than: 0}
+  validates :medium, comparison: { greater_than_or_equal_to: :minimum}
+  validates :maximum, comparison: { greater_than_or_equal_to: :medium}
 end
--- spec/models/reference_tables/degree_spec.rb
+++ spec/models/reference_tables/degree_spec.rb
@@ -24,4 +24,16 @@ RSpec.describe ReferenceTables::Degree, type: :model do
   it 'is not valid without a maximum' do
     expect(ReferenceTables::Degree.new(number: 1, minimum: 91, medium: 100, hay_degrees: 9,  ggs_degrees: 4)).not_to be_valid
   end
+
+  it 'is not valid with a negative degree' do
+    expect(ReferenceTables::Degree.new(number: -1, minimum: 91, medium: 100, maximum: 200, hay_degrees: 9,  ggs_degrees: 4)).not_to be_valid
+  end
+
+  it 'is not valid with medidum bigger than minimum' do
+    expect(ReferenceTables::Degree.new(number: 1, minimum: 115, medium: 100, maximum: 123, hay_degrees: 9,  ggs_degrees: 4)).not_to be_valid
+  end
+
+  it 'is not valid with meidum bigger than minimum' do
+    expect(ReferenceTables::Degree.new(number: 1, minimum: 115, medium: 100, maximum: 80, hay_degrees: 9,  ggs_degrees: 4)).not_to be_valid
+  end
 end
